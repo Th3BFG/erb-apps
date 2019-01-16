6960985f1c4c773ed7ab3487a196890fef46d009
Th3BFG <th3bfg@gmail.com>
2019-01-15 20:59:50 -0500

    Expanded code coverage on Header to > 90%

--- erb-apps/src/app/header/header.component.spec.ts
+++ erb-apps/src/app/header/header.component.spec.ts
@@ -1,10 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
 import { MatModule } from '../mat.module';
+import { Router } from '@angular/router';
+
+class MockRouter {
+  navigate(url: string) {}
+}
 
 describe('HeaderComponent', () => {
     beforeEach(async(() => {
+      const router = {
+        navigate: jasmine.createSpy('navigate')
+      };
       TestBed.configureTestingModule({
         imports: [
           RouterTestingModule,
@@ -13,6 +21,9 @@ describe('HeaderComponent', () => {
         declarations: [
           HeaderComponent
         ],
+        providers: [
+          { provide: Router, useClass: MockRouter}
+        ]
       }).compileComponents();
     }));
 
@@ -35,4 +46,31 @@ describe('HeaderComponent', () => {
       const compiled = fixture.debugElement.nativeElement;
       expect(compiled.querySelector('div img')).toBeTruthy();
     });
+
+    it('should route to "/blog" with goHome', inject([Router], (router: Router) => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      const component = fixture.componentInstance;
+      fixture.detectChanges();
+      const spy = spyOn(router, 'navigate');
+      component.goHome();
+      expect(spy).toHaveBeenCalledWith(['blog']);
+    }));
+
+    it('should route to "/downloads" with goToDownloads', inject([Router], (router: Router) => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      const component = fixture.componentInstance;
+      fixture.detectChanges();
+      const spy = spyOn(router, 'navigate');
+      component.goToDownloads();
+      expect(spy).toHaveBeenCalledWith(['downloads']);
+    }));
+
+    it('should route to "/about" with goToAbout', inject([Router], (router: Router) => {
+      const fixture = TestBed.createComponent(HeaderComponent);
+      const component = fixture.componentInstance;
+      fixture.detectChanges();
+      const spy = spyOn(router, 'navigate');
+      component.goToAbout();
+      expect(spy).toHaveBeenCalledWith(['about']);
+    }));
 });
