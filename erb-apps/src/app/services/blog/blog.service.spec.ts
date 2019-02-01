d8ea1e1d4dd8b8648c74aae0c295a8a49f9cd414
Th3BFG <th3bfg@gmail.com>
2019-02-01 18:09:09 -0500

    Expanded test cases for full coverage of BlogService

--- erb-apps/src/app/services/blog/blog.service.spec.ts
+++ erb-apps/src/app/services/blog/blog.service.spec.ts
@@ -42,8 +42,29 @@ describe('BlogService', () => {
       _service.getBlogPost(2).subscribe();
       const req = _httpMock.expectOne(`${_service.API_URL}/posts/2`);
       expect(req.request.method).toBe('GET');
-      // Verify last character is param supplied
-      expect(req.request.url.split('/').pop()).toBe('2');
       req.flush(POSTS);
-  });
+    });
+
+    it('should make a POST request to /posts with a body', () => {
+      _service.insertBlogPost(POSTS[0]).subscribe();
+      const req = _httpMock.expectOne(`${_service.API_URL}/posts`);
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toBe(POSTS[0]);
+      req.flush(POSTS);
+    });
+
+    it('should make a PUT request to /posts with one parameter and a body', () => {
+      _service.updateBlogPost(POSTS[0]).subscribe();
+      const req = _httpMock.expectOne(`${_service.API_URL}/posts/2`);
+      expect(req.request.method).toBe('PUT');
+      expect(req.request.body).toBe(POSTS[0]);
+      req.flush(POSTS);
+    });
+
+    it('should make a DELETE request to /posts with one parameter', () => {
+      _service.deleteBlogPost(2).subscribe();
+      const req = _httpMock.expectOne(`${_service.API_URL}/posts/2`);
+      expect(req.request.method).toBe('DELETE');
+      req.flush(POSTS);
+    });
 });
